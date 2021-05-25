36b2c1a4025478e83665b59dc9e47020bc9751ac
liangyi <liangyi@liangyi-pc>
2021-05-25 16:01:07 +0800

    若 tmp 与 dst 目录项数一项，使用相同下标项

--- src/plotman/manager.py
+++ src/plotman/manager.py
@@ -99,16 +99,21 @@ def maybe_start_new_plot(dir_cfg, sched_cfg, plotting_cfg):
         else:
             # Plot to oldest tmpdir.
             tmpdir = max(rankable, key=operator.itemgetter(1))[0]
-
-            # Select the dst dir least recently selected
-            dir2ph = { d:ph for (d, ph) in dstdirs_to_youngest_phase(jobs).items()
-                      if d in dir_cfg.dst and ph is not None}
-            unused_dirs = [d for d in dir_cfg.dst if d not in dir2ph.keys()]
-            dstdir = ''
-            if unused_dirs: 
-                dstdir = random.choice(unused_dirs)
+            
+            # 若 tmp 与 dst 目录项数一项，使用相同下标项
+            if len(dir_cfg.tmp) == len(dir_cfg.dst) :
+                idx = dir_cfg.tmp.index(tmpdir)
+                dstdir = dir_cfg.dst[idx]
             else:
-                dstdir = max(dir2ph, key=dir2ph.get)
+                # Select the dst dir least recently selected
+                dir2ph = { d:ph for (d, ph) in dstdirs_to_youngest_phase(jobs).items()
+                          if d in dir_cfg.dst and ph is not None}
+                unused_dirs = [d for d in dir_cfg.dst if d not in dir2ph.keys()]
+                dstdir = ''
+                if unused_dirs: 
+                    dstdir = random.choice(unused_dirs)
+                else:
+                    dstdir = max(dir2ph, key=dir2ph.get)
 
             logfile = os.path.join(
                 dir_cfg.log, pendulum.now().isoformat(timespec='microseconds').replace(':', '_') + '.log'
